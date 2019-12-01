7bc79d29228b29af88659a4e79fa7fc5ac520221
L'inspecteur <48238287+Linspecteur@users.noreply.github.com>
2019-12-01 17:56:37 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,5 +9,24 @@ client.login(process.env.token); //Token (Série de chiffre) propre a chaque Bot
 
 client.on("ready", () => { //Signifie que le bot à bien démarré
     console.log("Je suis prêt !"); //Lorsque que le bot est lancé observer la console Visual Studio
-    client.user.setGame("L'ELITE🌟 | 20 invites = 1 Nitro"); //Voir le Jeu sur le Discord
+    client.user.setGame("L'ELITE🌟\nJ'ai rejoinds le canal."| 20 invites = 1 Nitro"); //Voir le Jeu sur le Discord
 });
+
+client.on('message', message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+  
+    if (message.content === '+join') {
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+          .then(connection => { // Connection is an instance of VoiceConnection
+            message.reply("L'ELITE🌟\nJ'ai rejoinds le canal.");
+          })
+          .catch(console.log);
+      } else {
+        message.reply("L'ELITE🌟\nTu dois etre dans un canal.");
+      }
+    }
+  });
